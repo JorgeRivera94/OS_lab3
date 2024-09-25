31db88b5e8ce5670eba34ffb1644551debe4abd1
Jorge Rivera <112115387+JorgeRivera94@users.noreply.github.com>
2024-09-25 18:11:10 -0400

    progressed Q2

---
+++ exercise_2/incude/tangent.h
@@ -0,0 +1,8 @@
+/**
+ * @brief Fills an array of of 1,000,000 elements with the tangent value of the
+ * integers between [1 , 1.000.000].
+ *
+ * TODO Go into details
+ * @param arr Pointer to an int array of size 1,000,000
+ */
+void FillArray(int* arr[1000000]);
---
+++ exercise_2/src/tangent.c
@@ -0,0 +1,7 @@
+#include <math.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include "threads.h"
